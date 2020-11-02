71ef1361e3fee37ba9aad7baf73458d45165e857
Arbaz <ansariarbaz720@gmail.com>
2020-11-02 18:49:01 +0530

    Created navbar and homepage

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello")
+let burger=document.querySelector(".burger");
+let navbar=document.querySelector(".navbar")
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('v-list');
+ 
+})
+
